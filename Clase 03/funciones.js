9cb489bc5088c6cbc8a8f4d9504d4b2ca9533778
nicolasnet <niconetgomez@hotmail.com>
2018-09-12 19:43:22 -0300

    cambios visibilidad

--- Clase 03/funciones.js
+++ Clase 03/funciones.js
@@ -23,7 +23,7 @@ function sumar(){
         var pDescripcion = $("pDescripcion");
         pDescripcion.innerHTML = "Ya puede ver el resultado.";
     }else{
-        alert("no sumo");
+        alert("no sumó");
     }
 
 
@@ -43,19 +43,44 @@ function sumarGuardar(){
     resultado.value = parseFloat(num1.value) + parseFloat(num2.value);
     var pDescripcion = $("pDescripcion"); //este tag es el parrafo de inicio
     pDescripcion.innerHTML = "Ya puede ver el resultado."; //se accede al texto entre el tag d inicio y final de este tag
-
+   
+    
     var tabla = $("tbody");
+    /*
+    var copianum1 = num1.value;
+    var copianum2 = num2.value;
+    var copiaresult = resultado.value;
 
-    tabla.innerHTML+= "<tr><td>"+num1.value+"</td>"+"<td>"+num2.value+"</td>"+"<td>"+resultado.value+"</td>"+"<td><input type='button' value='Mostrar' onclick='mostrar(num1.value,num2.value,resultado.value)' class='button'></td></tr>";
+    var i = 0;
+    
+    alert(i);
+    var matriz = new Array();
+    matriz[i].push = (copianum1, copianum2, copiaresult);
+    alert(matriz[i][0]);
+    */
+
+
+    tabla.innerHTML+= "<tr><td>"+num1.value+"</td>"+"<td>"+num2.value+"</td>"+"<td>"+resultado.value+"</td>"+"<td><input type='button' value='Mostrar' onclick='mostrar(num1.value, num2.value, resultado.value)' class='button'></td></tr>";
     
 }
 
 function mostrar(valor1, valor2, valorresultado){
-    num1 = $("num1");
-    num2 = $("num2");
-    resultado = $("resultado");
+    var num1 = $("num1");
+    var num2 = $("num2");
+    var resultado = $("resultado");
 
-    num1.value = valor2;
+    num1.value = valor1;
     num2.value = valor2;
     resultado.value = valorresultado;
+}
+
+
+function Visible(){
+    var fieldset = $("inputs");
+    fieldset.className = "fieldsetVisible";
+}
+
+function NoVisible(){
+    var fieldset = $("inputs");
+    fieldset.className = "fieldsetNOVisible";
 }
